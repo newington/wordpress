10ff5796b128d012772c8251668697fa4bd57cf7
Myles Carrick <myles@mylescarrick.com>
2014-11-12 08:15:21 +1100

    batch create plugin

---
+++ wp-content/plugins/batch-create/batch-create.php
@@ -0,0 +1,158 @@
+<?php
+/*
+Plugin Name: Batch Create
+Plugin URI: http://premium.wpmudev.org/project/batch-create
+Description: Create hundred or thousands of blogs and users automatically by simply uploading a csv text file - subdomain and user creation automation has never been so easy.
+Author: WPMU DEV
+Text Domain: batch_create
+Version: 1.5
+Network: true
+Author URI: http://premium.wpmudev.org/
+WDP ID: 84
+*/
+
+/**
+ * The main class of the plugin
+ */
+
+class Incsub_Batch_Create {
+
+	// The version slug for the DB
+	public static $version_option_slug = 'incsub_batch_create_version';
+
+	// Admin pages. THey could be accesed from other points
+	// So they're statics
+	static $network_main_menu_page;
+
+	static $batch_creator;
+
+	public function __construct() {
+		$this->set_globals();
+
+		$this->includes();
+
+		add_action( 'init', array( &$this, 'maybe_upgrade' ) );
+
+		add_action( 'init', array( &$this, 'init_plugin' ) );
+
+		add_action( 'plugins_loaded', array( &$this, 'load_text_domain' ) );
+
+	}
+
+
+
+
+	/**
+	 * Set the plugin constants
+	 */
+	private function set_globals() {
+
+		//TODO: Change the constant names
+
+		// Basics
+		define( 'INCSUB_BATCH_CREATE_VERSION', '1.5' );
+		define( 'INCSUB_BATCH_CREATE_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+		define( 'INCSUB_BATCH_CREATE_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+		define( 'INCSUB_BATCH_CREATE_PLUGIN_FILE_DIR', plugin_dir_path( __FILE__ ) . 'batch-create.php' );
+
+		// Language domain
+		define( 'INCSUB_BATCH_CREATE_LANG_DOMAIN', 'incsub_batch_create' );
+
+		// URLs
+		define( 'INCSUB_BATCH_CREATE_ASSETS_URL', INCSUB_BATCH_CREATE_PLUGIN_URL . 'assets/' );
+
+		// Dirs
+		define( 'INCSUB_BATCH_CREATE_ADMIN_DIR', INCSUB_BATCH_CREATE_PLUGIN_DIR . 'admin/' );
+		define( 'INCSUB_BATCH_CREATE_MODEL_DIR', INCSUB_BATCH_CREATE_PLUGIN_DIR . 'model/' );
+		define( 'INCSUB_BATCH_CREATE_INCLUDES_DIR', INCSUB_BATCH_CREATE_PLUGIN_DIR . 'inc/' );
+
+	}
+
+	/**
+	 * Include files needed
+	 */
+	private function includes() {
+		// Model
+		require_once( INCSUB_BATCH_CREATE_MODEL_DIR . 'model.php' );
+
+		// Libraries
+		require_once( INCSUB_BATCH_CREATE_INCLUDES_DIR . 'admin-page.php' );
+		require_once( INCSUB_BATCH_CREATE_INCLUDES_DIR . 'errors-handler.php' );
+		require_once( INCSUB_BATCH_CREATE_INCLUDES_DIR . 'helpers.php' );
+		require_once( INCSUB_BATCH_CREATE_INCLUDES_DIR . 'integration.php' );
+
+		// Admin Pages
+		require_once( INCSUB_BATCH_CREATE_ADMIN_DIR . 'pages/network-main-page.php' );
+		require_once( INCSUB_BATCH_CREATE_ADMIN_DIR . 'tables/queue-table.php' );
+
+		global $wpmudev_notices;
+		$wpmudev_notices[] = array( 'id'=> 84,'name'=> 'Batch Create', 'screens' => array( 'settings_page_batch-create-menu-network' ) );
+		include_once( 'externals/wpmudev-dash-notification.php' );
+	}
+
+	/**
+	 * Upgrade the plugin when a new version is uploaded
+	 */
+	public function maybe_upgrade() {
+		$current_version = get_site_option( self::$version_option_slug );
+
+		if ( $current_version == INCSUB_BATCH_CREATE_VERSION )
+			return;
+
+		if ( ! $current_version )
+			$current_version = '1.3.3'; // This is the first version that includes some upgradings
+
+		// For the second version, we're just saving the version in DB
+		if ( version_compare( $current_version, '1.4', '<' ) ) {
+			require_once( INCSUB_BATCH_CREATE_INCLUDES_DIR . 'upgrade.php' );
+			batch_create_upgrade_14();
+		}
+
+		update_site_option( self::$version_option_slug, INCSUB_BATCH_CREATE_VERSION );
+	}
+
+
+	/**
+	 * Load the plugin text domain and MO files
+	 *
+	 * These can be uploaded to the main WP Languages folder
+	 * or the plugin one
+	 */
+	public function load_text_domain() {
+		$locale = apply_filters( 'plugin_locale', get_locale(), INCSUB_BATCH_CREATE_LANG_DOMAIN );
+
+		load_textdomain( INCSUB_BATCH_CREATE_LANG_DOMAIN, WP_LANG_DIR . '/' . INCSUB_BATCH_CREATE_LANG_DOMAIN . '/' . INCSUB_BATCH_CREATE_LANG_DOMAIN . '-' . $locale . '.mo' );
+		load_plugin_textdomain( INCSUB_BATCH_CREATE_LANG_DOMAIN, false, dirname( plugin_basename( __FILE__ ) ) . '/lang/' );
+	}
+
+	/**
+	 * Initialize the plugin
+	 */
+	public function init_plugin() {
+		global $wpdb;
+
+		require_once( INCSUB_BATCH_CREATE_INCLUDES_DIR . 'creator.php' );
+
+		$wpdb->batch_create_queuemeta = $wpdb->base_prefix . 'batch_create_queuemeta';
+
+		// A network menu
+		$args = array(
+			'parent' => 'settings.php',
+			'menu_title' => __( 'Batch Create', INCSUB_BATCH_CREATE_LANG_DOMAIN ),
+			'page_title' => __( 'Batch Create', INCSUB_BATCH_CREATE_LANG_DOMAIN ),
+			'network_menu' => true,
+			'screen_icon_slug' => 'batch-create',
+			'tabs' => array(
+				'upload' => __( 'Batch Create', INCSUB_BATCH_CREATE_LANG_DOMAIN ),
+				'queue' => __( 'Current queue', INCSUB_BATCH_CREATE_LANG_DOMAIN ),
+				'log-file' => __( 'Log File', INCSUB_BATCH_CREATE_LANG_DOMAIN )
+
+			)
+		);
+		self::$network_main_menu_page = new Batch_Create_Network_Main_Menu( 'batch-create-menu', 'manage_network', $args );
+
+		self::$batch_creator = new Incsub_Batch_Create_Creator();
+	}
+}
+
+new Incsub_Batch_Create();
---
+++ wp-content/plugins/batch-create/model/model.php
@@ -0,0 +1,225 @@
+<?php
+
+/**
+ * The main model class.
+ *
+ * Please, do not spread your queries through your
+ * code, group your queries here.
+ *
+ * You can create new classes for different models
+ */
+
+class Incsub_Batch_Create_Model {
+
+	static $instance;
+
+	// This option will tell WP if the schema has been created
+	// Instead of using the activation hook, we'll use this
+	// TODO: Change slug
+	public $schema_created_option_slug = 'batch_create_installed';
+
+	// Tables names
+	private $queue;
+	private $queue_meta;
+
+	// Charset and Collate
+	private $db_charset_collate;
+
+
+	/**
+	 * Return an instance of the class
+	 *
+	 * @since 0.1
+	 *
+	 * @return Object
+	 */
+	public static function get_instance() {
+		if ( self::$instance === null )
+			self::$instance = new self();
+
+        return self::$instance;
+	}
+
+	/**
+	 * Set the tables names, charset, collate and creates the schema if needed.
+	 * This way, the schema will be created when the model is created for first time.
+	 */
+	protected function __construct() {
+		global $wpdb;
+
+		// TODO: Change tables names
+		$this->queue = $wpdb->base_prefix . 'batch_create_queue';
+		$this->queue_meta = $wpdb->base_prefix . 'batch_create_queuemeta';
+
+		// Get the correct character collate
+        $db_charset_collate = '';
+        if ( ! empty($wpdb->charset) )
+          $this->db_charset_collate = "DEFAULT CHARACTER SET $wpdb->charset";
+        if ( ! empty($wpdb->collate) )
+          $this->db_charset_collate .= " COLLATE $wpdb->collate";
+
+      	// Have we created the DB schema?
+      	if ( false === get_site_option( $this->schema_created_option_slug ) ) {
+      		$this->create_schema();
+
+      		update_site_option( $this->schema_created_option_slug, true );
+      	}
+	}
+
+	/**
+	 * Create the required DB schema
+	 *
+	 * @since 0.1
+	 */
+	public function create_schema() {
+		$this->create_queue_table();
+		$this->create_queue_meta_table();
+	}
+
+	/**
+	 * Create the table 1
+	 * @return type
+	 */
+	private function create_queue_table() {
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		$sql = "CREATE TABLE $this->queue (
+				  batch_create_ID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+				  batch_create_site bigint(20) DEFAULT NULL,
+				  batch_create_blog_name varchar(255) NOT NULL DEFAULT 'null',
+				  batch_create_blog_title varchar(255) NOT NULL DEFAULT 'null',
+				  batch_create_user_name varchar(255) NOT NULL DEFAULT 'null',
+				  batch_create_user_pass varchar(255) NOT NULL DEFAULT 'null',
+				  batch_create_user_email varchar(255) NOT NULL DEFAULT 'null',
+				  batch_create_user_role varchar(255) NOT NULL DEFAULT 'null',
+				  PRIMARY KEY  (batch_create_ID)
+				) ENGINE=InnoDB $this->db_charset_collate;";
+
+        dbDelta($sql);
+	}
+
+	private function create_queue_meta_table() {
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+		$sql = "CREATE TABLE IF NOT EXISTS $this->queue_meta (
+				  meta_id bigint(20) NOT NULL AUTO_INCREMENT,
+				  batch_create_queue_id bigint(20) NOT NULL,
+				  meta_key varchar(255) DEFAULT NULL,
+				  meta_value longtext,
+				  PRIMARY KEY  (meta_id),
+				  KEY batch_create_queue_id (batch_create_queue_id),
+  				  KEY meta_key (meta_key)
+				) ENGINE=InnoDB $this->db_charset_collate;";
+
+        dbDelta($sql);
+	}
+
+	/**
+	 * Drop the schema
+	 */
+	public function delete_tables() {
+		global $wpdb;
+
+		$wpdb->query( "DROP TABLE $this->queue;" );
+		$wpdb->query( "DROP TABLE $this->queue_meta;" );
+	}
+
+
+	public function count_queue_items() {
+		global $wpdb, $current_site;
+
+		$result = $wpdb->get_var(
+			$wpdb->prepare(
+				"SELECT COUNT(*) FROM $this->queue WHERE batch_create_site = %d", $current_site->id
+			)
+		);
+
+		return absint( $result );
+	}
+
+
+	/**
+	 * Add entry into the database
+	 *
+	 * Since Batch Create 1.1.0
+	 */
+	function insert_queue( $args ) {
+		global $wpdb, $current_site;
+
+		array_unshift( $args, $current_site->id );
+
+		$args[2] = iconv( "Windows-1252", "UTF-8", $args[2] );
+
+		$wpdb->insert(
+			$this->queue,
+			array(
+				'batch_create_site'			=> $args[0],
+				'batch_create_blog_name'	=> $args[1],
+				'batch_create_blog_title'	=> $args[2],
+				'batch_create_user_name'	=> $args[3],
+				'batch_create_user_pass'	=> $args[4],
+				'batch_create_user_email'	=> $args[5],
+				'batch_create_user_role'	=> $args[6],
+			),
+			array(  '%d', '%s', '%s', '%s', '%s', '%s', '%s' )
+		);
+
+		return $wpdb->insert_id;
+
+	}
+
+	function clear_queue() {
+		$queue_item = $this->get_queue_item();
+		while( ! empty( $queue_item ) ) {
+			$this->delete_queue_item( $queue_item->batch_create_ID );
+			$queue_item = $this->get_queue_item();
+		}
+	}
+
+	public function get_queue_item() {
+		global $wpdb, $current_site;
+
+		return $wpdb->get_row(
+			$wpdb->prepare( "SELECT * FROM $this->queue WHERE batch_create_site = %d", $current_site->id )
+		);
+	}
+
+
+	public function delete_queue_item( $id ) {
+		global $wpdb;
+
+		$wpdb->query(
+			$wpdb->prepare(
+				"DELETE FROM $this->queue WHERE batch_create_ID = %d",
+				$id
+			)
+		);
+
+		$meta_keys = $wpdb->get_col(
+			$wpdb->prepare(
+				"SELECT meta_key FROM $this->queue_meta WHERE batch_create_queue_id = %d",
+				$id
+			)
+		);
+
+		if ( ! empty( $meta_keys ) ) {
+			foreach ( $meta_keys as $meta_key ) {
+				batch_create_delete_queue_meta( $id, $meta_key );
+			}
+		}
+	}
+
+	public function get_queue_items( $current_page, $per_page ) {
+		global $wpdb, $current_site;
+
+		return $wpdb->get_results(
+			$wpdb->prepare(
+				"SELECT * FROM $this->queue WHERE batch_create_site = %d LIMIT %d, %d",
+				$current_site->id,
+				intval( ( $current_page - 1 ) * $per_page),
+				intval( $per_page )
+			), ARRAY_A
+		);
+	}
+
+}
