3190109c776faf88e0e04ef09dc57893a6ffb0ea
Myles Carrick <myles@mylescarrick.com>
2014-11-13 12:13:37 +1100

    installed gd-bbpress-attachments 2.1

---
+++ wp-content/plugins/gd-bbpress-attachments/forms/tabs/d4p.php
@@ -0,0 +1,31 @@
+<div class="d4p-information">
+    <div class="d4p-plugin">
+        <h3>GD Products Center</h3>
+        <a target="_blank" href="http://www.gdpc.me/"><?php _e("Plugin Home Website", "gd-bbpress-attachments"); ?></a><br/>
+        <a target="_blank" href="http://www.dev4press.com/plugins/gd-products-center/"><?php _e("Home on Dev4Press", "gd-bbpress-attachments"); ?></a>
+    </div>
+    <div class="d4p-plugin" style="margin-top: 1em;">
+        <h3>GD Custom Posts And Taxonomies Tools</h3>
+        <a target="_blank" href="http://www.gdcpttools.com/"><?php _e("Plugin Home Website", "gd-bbpress-attachments"); ?></a><br/>
+        <a target="_blank" href="http://www.dev4press.com/plugins/gd-taxonomies-tools/"><?php _e("Home on Dev4Press", "gd-bbpress-attachments"); ?></a>
+    </div>
+    <div class="d4p-plugin" style="margin-top: 1em;">
+        <h3>GD Press Tools</h3>
+        <a target="_blank" href="http://www.dev4press.com/plugins/gd-press-tools/"><?php _e("Home on Dev4Press", "gd-bbpress-attachments"); ?></a>
+    </div>
+</div>
+<div class="d4p-information-second">
+    <h3><?php _e("Other Premium Plugins", "gd-bbpress-attachments"); ?></h3>
+    <a target="_blank" href="http://www.dev4press.com/plugins/gd-azon-fusion/">GD aZon FUSION</a><br/>
+    <h3><?php _e("Premium Themes", "gd-bbpress-attachments"); ?></h3>
+    <a target="_blank" href="http://www.dev4press.com/themes/">List of Themes</a><br/>
+    <a target="_blank" href="http://www.dev4press.com/xscape/">xScape Theme Framework</a>
+    <h3><?php _e("Free Plugins", "gd-bbpress-attachments"); ?></h3>
+    <a target="_blank" href="http://www.dev4press.com/plugins/gd-pages-navigator/">GD Pages Navigator</a><br/>
+    <a target="_blank" href="http://www.dev4press.com/plugins/gd-unit-converter/">GD Unit Converter</a><br/>
+    <a target="_blank" href="http://www.dev4press.com/plugins/gd-linkedin-badge/">GD LinkedIn Badge</a>
+</div>
+<div class="d4p-clear"></div>
+<div class="d4p-copyright">
+    Dev4Press &copy; 2008 - 2014 <a target="_blank" href="http://www.dev4press.com">www.dev4press.com</a> | Golden Dragon WebStudio <a target="_blank" href="http://www.gdragon.info">www.gdragon.info</a>
+</div>
